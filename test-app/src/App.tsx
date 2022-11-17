29cc60c966ea0d07c52e739a03d6d2e1c383e6b6
Andreas Dausenau <d4us1@MBP-von-Andreas.fritz.box>
2022-11-17 16:45:16 +0100

    Added test app for trying the component.

---
+++ test-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ReMaWy} from "@d4us1/remawy";
+
+function App() {
+  return (
+    <div>
+      <ReMaWy markdown={'Coolio'} onSubmitChange={() => {}} />
+    </div>
+  );
+}
+
+export default App;
